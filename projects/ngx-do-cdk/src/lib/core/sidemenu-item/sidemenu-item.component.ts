ee50ca0ed37530624827cbbb89f5255f6d421523
Sierk Hoeksma <sierk@hoeksma.org>
2018-11-04 19:46:36 +0100

    Added Role Controll

--- projects/ngx-do-cdk/src/lib/core/sidemenu-item/sidemenu-item.component.ts
+++ projects/ngx-do-cdk/src/lib/core/sidemenu-item/sidemenu-item.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Router } from '@angular/router';
 import {MatTooltipModule} from '@angular/material/tooltip';
 import { CoreAuth} from '../core.auth';
 import { Observable,from } from 'rxjs';
@@ -15,10 +16,28 @@ export class SidemenuItemComponent implements OnInit {
     @Input() secondaryMenu = false;
     visibleRole : boolean = true;
 
-    constructor(private coreAuth:CoreAuth) { }
+    constructor(private coreAuth:CoreAuth,private router:Router) { }
 
     ngOnInit() {
-      this.coreAuth.hasRole(this.menu ? this.menu.role: null).then(state=>{
+      let role = this.menu ? this.menu.expectedRole: null;
+      //Check if the role is protected by path
+      if (role==null && this.menu && this.menu.link) { 
+        var routerRole = function(routes,link){
+          let links = link.split('/').splice(0,1);
+          let path = links[0];
+          routes.forEach(function(route) {
+            if (route['path']==links[0] || route['path']=='/'+links[0] ){
+              if (route['data'] && route['data']['expextedRole']) 
+                role=route['data']['expextedRole'];
+            }
+            let childLink = links.splice(0,1); //Remove the parent
+            if (route['children']) 
+              routerRole(route['children'],childLink.join('/'));
+          });
+        }
+        routerRole(this.router.config,this.menu.link);
+      }
+      this.coreAuth.hasRole(role).then(state=>{
         this.visibleRole=state;
       })
     }
