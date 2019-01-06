201b7850ae44d3934804f6c3052d8a849ea3b8a5
Sierk Hoeksma <sierk@hoeksma.org>
2019-01-06 16:45:37 +0100

    Added CRUD support

--- projects/ngx-do-proxy/ngx-do-proxy.js
+++ projects/ngx-do-proxy/ngx-do-proxy.js
@@ -52,6 +52,8 @@ var myOptions = Object.assign({
   adminList: [], //List with users which have by default admin rights
   encryptSystem: false, //Should we encrypt,decrypt system values
   allowAccessToken: ['admin'], //List with roles allowed to use access_token
+ // crudTables: null, //Array with crud Protected tables, or null when disabled
+  crudTables: ['test'], //Array with crud Protected tables, or null when disabled
   logFile: null, //When set with name the console log will be written here
   greenLock:{
       email: null // You MUST change this to a valid email address
@@ -442,8 +444,9 @@ function createServer(server,plugins){
   if (routes) server.use(jsonServer.rewriter(routes)) //Set the custom routes
   server.use(session({ 
     secret: myOptions.secretKey, 
-    resave: false,
-    saveUninitialized: true,
+    resave: true,
+    saveUninitialized: false,
+    unset: 'destroy',
     genid: function(req) {
       return uuidv4() // use UUIDs for session IDs
     },
@@ -662,6 +665,116 @@ function createServer(server,plugins){
     next();
   }) 
   
+  
+  //CRUD will deliver the crud data from AUTH table
+  server.use("/CRUD",(req,res,next)=>{
+    let id= req.url.substr(1);
+    let decode = decodeToken(req);
+    if (decode && isNaN(decode)){  
+       const db = app.db.getState() //Get the last active database
+       let admin = hasRole('admin',req),index;
+       if (id && admin){
+         index = db.auth ? db.auth.findIndex(auth => auth.id == id) : -1; 
+       } else {
+        index = db.auth ? db.auth.findIndex(auth => auth.login == decode['email']) : -1;
+       }
+       if (index>=0 && req.method=="GET") { //READ
+         let status = 200;
+         let rec = db.auth[index];
+         let message = {id:rec.id,crud:rec.crud || [],tables:myOptions.crudTables};
+         res.status(status).json(message) 
+        return; 
+       } else if (index>=0 && (req.method=="PUT" || req.method=="POST")) { //Save
+         let status = 200;
+         let rec = db.auth[index];
+         rec['crud']=req.body.crud;
+         rec['updatedAt']=req.body.updatedAt;
+         rec['updatedBy']=req.body.updatedBy;
+         db.auth[index]=rec;
+          server.db.write();
+         let message = {id:rec.id,crud:rec.crud || [],tables:myOptions.crudTables};
+         res.status(status).json(message) 
+         return;
+       }
+    }
+    let status = 403;
+    let message = "Error in CRUD request";
+    res.status(status).json(status,message)
+    return; //CRUD will always exist
+  }) 
+  
+  //Enforce crud protected tables
+  if (myOptions.crudTables){
+    server.use((req,res,next)=>{
+      let path = req.url.split('/');
+      let table = path[1];
+      let admin = hasRole('admin',req),index;
+      if (!admin && myOptions.crudTables.indexOf(table)>=0){
+        //Load the crud assigned to me
+        let decode = decodeToken(req);
+        if (decode && isNaN(decode)){ 
+          const db = app.db.getState() //Get the last active database
+          let email =  decode['email'];
+          index = db.auth ? db.auth.findIndex(auth => auth.login == email) : -1;
+          if (index>=0){ //Add Filter just my own records, or the once if have the rights
+            //Add filter created by
+            let allowed = [db.auth[index].id];
+            //Loop trough the auth to see if there are rights given to me
+            db.auth.forEach(function(rec){
+              if (rec.crud) {
+                rec.crud.forEach(function(crud){
+                  if (crud.user==email && crud.table==table) {
+                    if (req.method=="GET" && crud.CRUD.indexOf('r')>=0) allowed.push(rec.id)
+                    else if (req.method=="PUT" && crud.CRUD.indexOf('u')>=0) allowed.push(rec.id)
+                    else if (req.method=="POST" && crud.CRUD.indexOf('c')>=0) allowed.push(rec.id)
+                    else if (req.method=="DELETE" && crud.CRUD.indexOf('d')>=0) allowed.push(rec.id)
+                  }
+                })
+              }
+            })
+            //Check if we are seatching for singular
+            if (path.length>2){
+               req.query = Object.assign(req.query,{createdBy_like:allowed,id:path[2]});
+               req.signular=true;
+               path.splice(2,1);
+               req.originalUrl=req.url=path.join('/');
+              // console.log("URL",req.url,req.query);
+            } else {
+               req.query = Object.assign(req.query,{createdBy_like:allowed});
+            }
+          }
+        } else {
+          let status = 403;
+          let message = "Error in CRUD request";
+          res.status(status).json(status,message)
+          return; //CRUD will always exist
+        }
+      }
+      next(); 
+    }) 
+    
+    //Fix for crud to use singular, we catch the send
+    server.use((req, res, next) => {
+      const _send = res.send
+      res.send = function (body) {
+        var singular= require('url').parse(req.url, true).query['_singular'];
+        if (req.signular || singular=="1") {
+          try {
+            const json = JSON.parse(body)
+            if (Array.isArray(json)) {
+              if (json.length === 1) {
+                return _send.call(this, JSON.stringify(json[0]))
+              } else if (json.length === 0) {
+                return res.status(404).send('{}')
+              }
+            }
+          } catch (e) {}
+        }
+        return _send.call(this, body)
+      }
+      next()
+     });
+  }
   
   //TODO: Add Get Auth and Delete Auth --> they need role admin
   
