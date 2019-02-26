c2e085ec03328cafd5d69fe52d4cd6d3db238bd7
Sierk Hoeksma <sierk@hoeksma.org>
2019-02-26 07:31:03 +0100

    Replaced DeepMerge

--- projects/ngx-do-api-gateway/core/005-Swagger.plugin.js
+++ projects/ngx-do-api-gateway/core/005-Swagger.plugin.js
@@ -40,60 +40,10 @@ const swaggerBase = {
     "application/json"
   ]
 }
-var extend = function () {
-	// Variables
-	var extended = {};
-	var deep = false;
-	var i = 0;
-
-	// Check if a deep merge
-	if ( Object.prototype.toString.call( arguments[0] ) === '[object Boolean]' ) {
-	    deep = arguments[0];
-	    i++;
-	}
-
-	// Merge the object into the extended object
-	var merge = function (obj) {
-    //Check if object is array
-    
-    //This is object
-		for (var prop in obj) {
-			if (obj.hasOwnProperty(prop)) {
-				// If property is an object, merge properties
-				if (deep && Object.prototype.toString.call(obj[prop]) === '[object Object]') {
-					extended[prop] = extend(extended[prop], obj[prop]);
-				} else {
-					extended[prop] = obj[prop];
-				}
-			}
-		}
-	};
-
-	// Loop through each object and conduct a merge
-	for (; i < arguments.length; i++) {
-		merge(arguments[i]);
-	}
-	return extended;
-};
 
 //Check if this is a json file and not url 
 function isSWAGGER(s) {
   return !proxy.isURL(s) && /\.sw.json$/.test(s)
-}
-
-function parseSwagger(filepath,contentString){
-  if (!contentString || contentString==="") return {} 
-  var string = contentString.replace(/^({\s*})*|({\s*})*$/g, "")
-  string = string.replace(/}\s*({\s*})*\s*{/g, ",")
-  string = string.replace(/}\s*{/g, ",")
-  string = string.replace(/\/\*[\s\S]*?\*\/|([^\\:]|^)\/\/.*$/gm, '$1') //Strip comments
-  string = string.replace(/\r?\n/g, "") //Allow multiple lines
-  try {
-    return jsonic(string);
-  } catch (err) {
-    console.log("Error parsing swagger",filepath,err);
-    return {}
-  }
 }
 
 module.exports = {
@@ -108,7 +58,7 @@ module.exports = {
           var files =[];
           var processFile=function(filepath,content){
             count--;
-            if (content) files.push(parseSwagger(filepath,content));
+            if (content) files.push(proxy.jsonParse(content));
             if (count==0){
               var schema = Object.assign({},swaggerBase);
               files.forEach(obj=>{
@@ -118,8 +68,8 @@ module.exports = {
               if (proxy.options.logLevel>1) console.log("Swagger active on",proxy.options.swagger);  
               const opts = {
                 schema: schema,
-                validateRequest: true,
-                validateResponse: false,
+                validateRequest: proxy.options.swaggerValidateRequest==undefined ? true : proxy.options.swaggerValidateRequest,
+                validateResponse: proxy.options.swaggerValidateResponse==undefined ? false : proxy.options.swaggerValidateResponse,
                 
                 requestValidationFn: (req, data, errors,res,next) => {
                   const message=`failed request validation: ${req.method} ${req.originalUrl}\n ${util.inspect(errors)}`;
